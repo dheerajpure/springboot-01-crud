a4ff41294ca4bc52a04726916828a6d8d812e9ff
dheerajpure <dheeraj.pure.tech@gmail.com>
2020-10-01 19:07:35 -0500

    first commit

---
+++ src/main/java/dp/spboot/example/demo/service/PersonService.java
@@ -0,0 +1,25 @@
+package dp.spboot.example.demo.service;
+
+import dp.spboot.example.demo.dao.PersonDao;
+import dp.spboot.example.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PersonService {
+
+    private final PersonDao personDao;
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao){
+        this.personDao=personDao;
+    }
+    public int addPerson(Person person){
+        return personDao.insertPerson(person);
+    }
+    public List<Person> getAllPeople(){
+        return personDao.selectAllPeople();
+    }
+}
